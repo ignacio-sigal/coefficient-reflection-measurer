e12db03ebfee6a852903ab24d86bd3fcfc3439e9
Sigal, Ignacio <ignaciosigal24@gmail.com>
2023-07-21 18:26:15 -0300

    This solves pylint?

--- classes/plotter.py
+++ classes/plotter.py
@@ -1,7 +1,13 @@
+"""
+Plotter class
+"""
 from abc import ABC, abstractmethod
 
 
 class Plot(ABC):
+    """
+    This abc class is intended to be used when a plot must be done.
+    """
     @abstractmethod
     def __init__(self, *args):
         """
