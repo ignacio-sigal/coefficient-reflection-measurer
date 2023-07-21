34f81d270e8a154b9c3182d37eb230833fd463e1
Sigal, Ignacio <ignaciosigal24@gmail.com>
2023-07-21 18:25:28 -0300

    This solves pylint?

--- classes/plotter.py
+++ classes/plotter.py
@@ -7,21 +7,18 @@ class Plot(ABC):
         """
         Abstract method
         """
-        ...
 
     @abstractmethod
     def set_axis_limits(self, **kwargs):
         """
         Abstract method
         """
-        ...
 
     @abstractmethod
     def plot(self, **kwargs):
         """
         Abstract method
         """
-        ...
 
     @property
     @abstractmethod
@@ -29,4 +26,3 @@ class Plot(ABC):
         """
         Property
         """
-        ...
